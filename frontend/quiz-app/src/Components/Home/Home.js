d994372cf705da834fbeeaddd49631b57a6a6b9d
Abhay <bajaj.abhay02@gmail.com>
2022-07-15 20:11:13 +0530

    Frontend UI/UX Changes

--- frontend/quiz-app/src/Components/Home/Home.js
+++ frontend/quiz-app/src/Components/Home/Home.js
@@ -1,36 +1,83 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./Home.css";
 import Header from "./Header";
 import shape from "../../Images/shape.png";
-import quiz from "../../Images/quiz_prev_ui.png"
+import quiz from "../../Images/quiz_prev_ui.png";
 
 const Home = () => {
+  const [qid, setqid] = useState("");
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+
+  const qidChangeHandler = (e) => {
+    setqid(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setData({
+      quizId: qid,
+    });
+  };
+
+  useEffect(() => {
+    const sendData = async () => {
+      const res = await fetch("http://localhost:8000/quiz/:qid", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const quizRes = await res.json();
+
+      if (res.status === 400) {
+        navigate(`/quiz/${quizRes.qid}`)
+      }
+
+      if (res.status === 200) {
+        navigate(`/quiz/${quizRes.qid}`);
+      }
+    };
+
+    if (data !== null) sendData();
+  }, [data]);
+
   return (
     <main>
-      <div class="big-wrapper light">
-        <img src={shape} alt="" class="shape" />
+      <div className="big-wrapper light">
+        <img src={shape} alt="" className="shape" />
         <Header />
-        <div class="showcase-area">
-          <div class="container">
-            <div class="left">
-              <div class="big-title">
+        <div className="showcase-area">
+          <div className="container">
+            <div className="left">
+              <div className="big-title">
                 <h1>SignUp now to create you own quiz .</h1>
                 <h1>Start Exploring now !!</h1>
               </div>
-              <p class="text" type='input' >
-                Enter the ID of the quiz to enter the quiz
-              </p>
-              <div class="cta">
-                <Link to="#" class="btn">
-                  Start Quiz
-                </Link>
-              </div>
+              <form className="quiz-id-form" onSubmit={submitHandler}>
+                <div>
+                  <input
+                    type="text"
+                    value={qid}
+                    placeholder="Enter Quiz ID"
+                    onChange={qidChangeHandler}
+                    required
+                  />
+                  <input
+                    type="submit"
+                    value="Start Quiz"
+                    className="btn btn-form"
+                  />
+                </div>
+              </form>
             </div>
 
-            <div class="right">
-              <img src={quiz} alt="Quiz Image" class="person" />
+            <div className="right">
+              <img src={quiz} alt="Quiz Image" className="person" />
             </div>
           </div>
         </div>
